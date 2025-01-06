f4482f2cd07a44187adf1c07c123109603062c53
Paweł Kruszko <pawelkruszko@gmail.com>
2025-01-06 21:29:44 +0100

    Task model added

--- kanban/tasks/views.py
+++ kanban/tasks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tasks
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Tasks.objects.all()
+    serializer_class = TaskSerializer
