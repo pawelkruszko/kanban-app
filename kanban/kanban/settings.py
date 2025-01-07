eb23b290d1b96cc691574d6084447a2c9fe40eeb
Paweł Kruszko <pawelkruszko@gmail.com>
2025-01-07 12:24:15 +0100

    project reorganization

--- kanban/kanban/settings.py
+++ kanban/kanban/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks',
+    'kanban.tasks',
     'rest_framework',
 ]
 
--- kanban/kanban/urls.py
+++ kanban/kanban/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from tasks import views
+from kanban.tasks import views
 
 router = DefaultRouter()
 router.register(r'tasks', views.TaskViewSet)
