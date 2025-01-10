c160773fd0237e26066e08bb5452eb4307134778
Paweł Kruszko <pawelkruszko@gmail.com>
2025-01-10 15:16:05 +0100

    Html added

--- kanban/kanban/tasks/views.py
+++ kanban/kanban/tasks/views.py
@@ -1,22 +1,37 @@
 from django.shortcuts import render, redirect
-from .models import Task
 from .forms import TaskForm
+from .models import Task
 
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/add_task.html', {'form': form})
 
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
-def add_task(request):
-    if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            if request.htmx:
-                tasks = Task.objects.all()
-                return render(request, 'tasks/task_list.html', {'tasks': tasks})
-            return redirect('task_list')
+
+def change_status(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task.status == 'TODO':
+        task.status = 'IN_PROGRESS'
+    elif task.status == 'IN_PROGRESS':
+        task.status = 'DONE'
     else:
-        form = TaskForm()
-    return render(request, 'tasks/add_task.html', {'form': form})
+        task.status = 'TODO'
+    task.save()
+    return redirect('task_list')
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
--- kanban/kanban/urls.py
+++ kanban/kanban/urls.py
@@ -4,4 +4,6 @@ from kanban.tasks import views
 urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('add/', views.add_task, name='add_task'),
+    path('change_status/<int:task_id>/', views.change_status, name='change_status'),
+    path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
 ]
